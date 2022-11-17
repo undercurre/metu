960c3a1aa2eead3609cbedc3ecea604dce5a38ae
Mr.Li <63112788+undercurre@users.noreply.github.com>
2022-11-17 20:23:36 +0800

    feat: presets

--- presets/presets.ts
+++ presets/presets.ts
@@ -1,5 +1,9 @@
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 import Unocss from 'unocss/vite'
 
 export default [
+    vue(),
+    vueJsx(),
     Unocss(),
 ]
