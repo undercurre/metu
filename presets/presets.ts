a392c8a57db52bfb74f9337c64537a179b7175f8
Mr.Li <63112788+undercurre@users.noreply.github.com>
2022-11-18 19:50:42 +0800

    feat: presets更新

--- presets/presets.ts
+++ presets/presets.ts
@@ -1,9 +1,21 @@
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite';
 
 export default [
     vue(),
     vueJsx(),
     Unocss(),
+    AutoImport({
+        include: [
+            /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+            /\.vue$/, /\.vue\?vue/, // .vue
+            /\.md$/, // .md
+        ],
+        imports: [
+            'vue'
+        ],
+        dts: './src/types/auto-imports.d.ts',
+    })
 ]
