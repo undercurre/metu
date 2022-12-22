19599faaa2794ead3258a981228b3b3a452af658
Mr.Li <63112788+undercurre@users.noreply.github.com>
2022-12-22 10:07:30 +0800

    fix: vite.config中的src源目录映射

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import presets from './presets/presets';
+
+const resolve = (dir) => path.resolve(__dirname, dir);
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [...presets],
+    resolve: {
+        alias: [
+            {
+                find: '@',
+                replacement: resolve('./src')
+            }
+        ]
+    }
 });
